f2fac6a8873421d9c93aace14c6adda4ad25491f
Marko Grizelj <marko.grizelj03@gmail.com>
2025-02-14 08:58:11 +0100

    feat: reconnect

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,3 +1,4 @@
+use gloo_timers::future::TimeoutFuture;
 use std::collections::HashMap;
 
 use chrono::Utc;
@@ -62,62 +63,70 @@ pub fn NavBar() -> Element {
             WebsocketClientMessageData,
             oneshot::Sender<Result<WebsocketServerResData, String>>,
         )>| async move {
-            let mut message_requests: HashMap<Uuid, _> = HashMap::new();
+            loop {
+                let mut message_requests: HashMap<Uuid, _> = HashMap::new();
+                let token = USER().map(|x| x.token);
 
-            let token = USER().clone().map(|x| x.token);
+                if let Some(token) = token {
+                    let (mut ws, mut wsio) = WsMeta::connect(
+                        format!("ws://localhost:3030/ws/?jwt_token={}", token),
+                        None,
+                    )
+                    .await
+                    .unwrap();
 
-            if let Some(token) = token {
-                let (mut ws, mut wsio) =
-                    WsMeta::connect(format!("ws://localhost:3030/ws/?jwt_token={}", token), None)
-                        .await
-                        .unwrap();
+                    let mut evts = ws.observe(ObserveConfig::default()).await.unwrap();
 
-                let mut evts = ws.observe(ObserveConfig::default()).await.unwrap();
+                    let chats_request_id = Uuid::new_v4();
+                    wsio.send(WsMessage::Text(
+                        serde_json::to_string(&WebsocketClientMessage {
+                            id: chats_request_id,
+                            data: WebsocketClientMessageData::GetChats,
+                        })
+                        .unwrap(),
+                    ))
+                    .await
+                    .unwrap();
 
-                let chats_request_id = Uuid::new_v4();
-                wsio.send(WsMessage::Text(
-                    serde_json::to_string(&WebsocketClientMessage {
-                        id: chats_request_id,
-                        data: WebsocketClientMessageData::GetChats,
-                    })
-                    .unwrap(),
-                ))
-                .await
-                .unwrap();
+                    loop {
+                        let rrx = rx.next();
+                        let evtx = select(evts.next(), wsio.next());
 
-                loop {
-                    let rrx = rx.next();
-                    let evtx = select(evts.next(), wsio.next());
+                        match select(rrx, evtx).await {
+                            Either::Left((x, _)) => {
+                                if let Some((data, responder)) = x {
+                                    let id = Uuid::new_v4();
 
-                    match select(rrx, evtx).await {
-                        Either::Left((x, _)) => {
-                            if let Some((data, responder)) = x {
-                                let id = Uuid::new_v4();
+                                    let request = WebsocketClientMessage { id, data };
 
-                                let request = WebsocketClientMessage { id, data };
-
-                                if let Ok(_) = wsio
-                                    .send(WsMessage::Text(serde_json::to_string(&request).unwrap()))
-                                    .await
-                                {
-                                    message_requests.insert(id, responder);
+                                    if let Ok(_) = wsio
+                                        .send(WsMessage::Text(
+                                            serde_json::to_string(&request).unwrap(),
+                                        ))
+                                        .await
+                                    {
+                                        message_requests.insert(id, responder);
+                                    }
                                 }
                             }
-                        }
 
-                        Either::Right((Either::Left((x, _)), _)) => {
-                            tracing::info!("websocket event {:?}", x);
-                            break;
-                        }
+                            Either::Right((Either::Left((x, _)), _)) => {
+                                tracing::info!("websocket event {:?}", x);
 
-                        Either::Right((Either::Right((x, _)), _)) => {
-                            if let Some(Text(payload)) = x {
-                                if let Ok(message) =
-                                    serde_json::from_str::<WebsocketServerMessage>(&payload)
-                                {
-                                    match message {
-                                        WebsocketServerMessage::RequestResponse { id, data } => {
-                                            match data {
+                                // break here connection probably dead
+                                break;
+                            }
+
+                            Either::Right((Either::Right((x, _)), _)) => {
+                                if let Some(Text(payload)) = x {
+                                    if let Ok(message) =
+                                        serde_json::from_str::<WebsocketServerMessage>(&payload)
+                                    {
+                                        match message {
+                                            WebsocketServerMessage::RequestResponse {
+                                                id,
+                                                data,
+                                            } => match data {
                                                 Ok(WebsocketServerResData::GetChats(chats)) => {
                                                     *CHATS.write() = chats.clone();
                                                 }
@@ -127,35 +136,42 @@ pub fn NavBar() -> Element {
                                                         let _ = x.send(result);
                                                     }
                                                 }
+                                            },
+
+                                            WebsocketServerMessage::NewMessage(message) => {
+                                                let chats = &mut (*CHATS.write());
+                                                let chat_o = chats
+                                                    .iter_mut()
+                                                    .find(|x| x.id == message.chat_id);
+
+                                                if let Some(chat) = chat_o {
+                                                    let ts = message.created_at;
+
+                                                    chat.messages.push(message.clone());
+                                                    chat.last_message_ts = ts;
+                                                }
+
+                                                chats.sort_by(|a, b| {
+                                                    a.last_message_ts
+                                                        .cmp(&b.last_message_ts)
+                                                        .reverse()
+                                                })
                                             }
-                                        }
 
-                                        WebsocketServerMessage::NewMessage(message) => {
-                                            let chats = &mut (*CHATS.write());
-                                            let chat_o =
-                                                chats.iter_mut().find(|x| x.id == message.chat_id);
+                                            WebsocketServerMessage::UserJoined {
+                                                chat_id,
+                                                user,
+                                            } => {
+                                                let chats = &mut (*CHATS.write());
+                                                let chat_o =
+                                                    chats.iter_mut().find(|x| x.id == chat_id);
 
-                                            if let Some(chat) = chat_o {
-                                                let ts = message.created_at;
-
-                                                chat.messages.push(message.clone());
-                                                chat.last_message_ts = ts;
-                                            }
-
-                                            chats.sort_by(|a, b| {
-                                                a.last_message_ts.cmp(&b.last_message_ts).reverse()
-                                            })
-                                        }
-
-                                        WebsocketServerMessage::UserJoined { chat_id, user } => {
-                                            let chats = &mut (*CHATS.write());
-                                            let chat_o = chats.iter_mut().find(|x| x.id == chat_id);
-
-                                            if let Some(chat) = chat_o {
-                                                if let None =
-                                                    chat.users.iter().find(|x| x.id == user.id)
-                                                {
-                                                    chat.users.push(user.clone())
+                                                if let Some(chat) = chat_o {
+                                                    if let None =
+                                                        chat.users.iter().find(|x| x.id == user.id)
+                                                    {
+                                                        chat.users.push(user.clone())
+                                                    }
                                                 }
                                             }
                                         }
@@ -165,6 +181,9 @@ pub fn NavBar() -> Element {
                         }
                     }
                 }
+
+                // wait for reconnect
+                TimeoutFuture::new(10000).await;
             }
         },
     );
