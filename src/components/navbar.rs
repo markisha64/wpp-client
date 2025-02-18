2e19fcaebe7966e1713c80cf593a465aa9c29a3d
markisha64 <marko.grizelj03@gmail.com>
2025-02-18 19:49:15 +0100

    feat: logo

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -20,7 +20,27 @@ pub fn NavBar() -> Element {
             class: "fixed top-0 z-50 w-full border-b bg-gray-800 border-gray-700 text-white",
             div {
                 class: "w-full flex flex-wrap items-center justify-between px-3 py-3 lg:px-5 lg:pl-3",
-                "CHET",
+                svg {
+                    class: "w-7 h-7",
+                    xmlns: "http://www.w3.org/2000/svg",
+                    view_box: "0 0 200 200",
+                    path {
+                        d: "M100 30 A 70 70 0 0 0 100 170",
+                        fill: "none",
+                        stroke: "#808080",
+                        stroke_width: 17
+                    }
+                    text {
+                        x: 130,
+                        y: 130,
+                        font_family: "Arial, sans-serif",
+                        font_size: 75,
+                        font_weight: "bold",
+                        fill: "#2563eb",
+                        text_anchor: "middle",
+                        "HET"
+                    }
+                },
                 if display_login {
                     div {
                         class: "flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse",
