0a32fe934ee94d874e82fa5436b3a39bfbb8a66c
Marko Grizelj <marko.grizelj03@gmail.com>
2025-07-31 12:59:46 +0200

    refact: restyled login page

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -8,13 +8,15 @@ use crate::{components::navbar::Auth, route::Route, BACKEND_URL, USER};
 pub fn Login() -> Element {
     let mut email_signal = use_signal(|| "".to_string());
     let mut password_signal = use_signal(|| "".to_string());
-    let toast_handle = use_coroutine_handle::<anyhow::Error>();
 
     let email = email_signal();
     let password = password_signal();
 
+    let mut error_signal = use_signal(|| Option::<String>::None);
+
     let is_logged_in = USER().is_some();
     let navigator = use_navigator();
+    let error = error_signal();
 
     use_effect(move || {
         if is_logged_in {
@@ -24,103 +26,108 @@ pub fn Login() -> Element {
 
     rsx! {
         div {
-            class: "flex w-screen mt-20",
+            class: "flex min-h-screen items-center justify-center bg-gray-50",
             div {
-                class: "min-w-sm p-6 border border-gray-200 rounded-lg shadow-sm bg-gray-800 border-gray-700 flex-col mx-auto mt-2",
-                h5 {
-                    class: "mb-2 text-2xl font-bold tracking-tight text-gray-900 text-white",
+                class: "w-full max-w-md p-8 bg-white rounded shadow-md",
+                h1 {
+                    class: "text-2xl font-bold mb-6 text-center",
                     "Login"
                 }
                 form {
-                    div {
-                        label {
-                            r#for: "email",
-                            class: "block mb-2 text-sm font-medium text-gray-900 text-white",
-                            "Email"
-                        }
-                        input {
-                            r#type: "text",
-                            id: "email",
-                            class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500",
-                            required: 1,
-                            onchange: move |evt| {
-                                email_signal.set(evt.value());
+                    class: "space-y-4",
+                    onsubmit: move |_| {
+                        to_owned![email, password];
+
+                        spawn(async move {
+                            let task: Result<(), anyhow::Error> = async move {
+                                let client = reqwest::Client::new();
+                                let res = client.post(format!("{}/user/login", BACKEND_URL))
+                                    .json(&LoginRequest {
+                                        email,
+                                        password,
+                                    })
+                                    .send()
+                                    .await?
+                                    .error_for_status()?
+                                    .json::<AuthResponse>()
+                                    .await?;
+
+                                let mut validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
+                                validation.insecure_disable_signature_validation();
+
+                                let key = DecodingKey::from_secret(&[]);
+                                let payload = jsonwebtoken::decode::<Claims>(res.token.as_str(), &key, &validation)?;
+
+                                *USER.write() = Some(Auth {
+                                    claims: payload.claims,
+                                    token: res.token.clone(),
+                                });
+
+                                web_sys::window()
+                                    .context("failed to get window")?
+                                    .local_storage()
+                                    .map_err(|_| anyhow!("failed to get local storage"))?
+                                    .context("failed to get storage")?
+                                    .set_item("jwt_token", res.token.as_str())
+                                    .map_err(|_| anyhow!("failed to get local storage"))?;
+
+                                navigator.replace(Route::Home);
+
+                                Ok(())
+                            }.await;
+
+                            if let Err(err) = task {
+                                error_signal.set(Some(err.to_string()));
                             }
-                        }
-                    }
+                        });
+                    },
                     div {
                         label {
-                            r#for: "password",
-                            class: "block mb-2 text-sm font-medium text-gray-900 text-white",
-                            "Password"
+                            class: "block mb-1 font-medium",
+                            "Email"
+                        },
+                        input {
+                            r#type: "email",
+                            class: "w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300",
+                            onchange: move|evt| {
+                                email_signal.set(evt.value());
+                            },
+                            required: "true"
                         }
+                    },
+                    div {
+                        label {
+                            class: "block mb-1 font-medium",
+                            "Password"
+                        },
                         input {
                             r#type: "password",
-                            id: "password",
-                            class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500",
-                            required: 1,
-                            onchange: move |evt| {
+                            class: "w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300",
+                            onchange: move|evt| {
                                 password_signal.set(evt.value());
-                            }
+                            },
+                            required: "true"
+                        }
+                    },
+                    if let Some(err) = error {
+                        div {
+                            class: "text-red-500 text-sm",
+                            "{err}"
                         }
                     }
-                    div {
-                        class: "flex flex-wrap justify-between gap-6 align-middle",
-                        Link {
-                            class: "block text-white text-sm py-2 px-4 underline",
-                            to: Route::Register {}, "Or, alternatively, register!"
-                        },
-                        button {
-                            r#type: "button",
-                            class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 float-right m-2",
-                            onclick: move |_| {
-                                to_owned![email, password];
-
-                                spawn(async move {
-                                    let task: Result<(), anyhow::Error> = async move {
-                                        let client = reqwest::Client::new();
-                                        let res = client.post(format!("{}/user/login", BACKEND_URL))
-                                            .json(&LoginRequest {
-                                                email,
-                                                password,
-                                            })
-                                            .send()
-                                            .await?
-                                            .error_for_status()?
-                                            .json::<AuthResponse>()
-                                            .await?;
-
-                                        let mut validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
-                                        validation.insecure_disable_signature_validation();
-
-                                        let key = DecodingKey::from_secret(&[]);
-                                        let payload = jsonwebtoken::decode::<Claims>(res.token.as_str(), &key, &validation)?;
-
-                                        *USER.write() = Some(Auth {
-                                            claims: payload.claims,
-                                            token: res.token.clone(),
-                                        });
-
-                                        web_sys::window()
-                                            .context("failed to get window")?
-                                            .local_storage()
-                                            .map_err(|_| anyhow!("failed to get local storage"))?
-                                            .context("failed to get storage")?
-                                            .set_item("jwt_token", res.token.as_str())
-                                            .map_err(|_| anyhow!("failed to get local storage"))?;
-
-                                        navigator.replace(Route::Home);
-
-                                        Ok(())
-                                    }.await;
-
-                                    if let Err(err) = task {
-                                        toast_handle.send(err);
-                                    }
-                                });
-                            },
-                            "Login"
-                        }
+                    button {
+                        r#type: "submit",
+                        class: "w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition",
+                        "Login"
+                    }
+                }
+                p {
+                    class: "mt-4 text-center text-sm",
+                    "Dont't have an account? ",
+                    Link {
+                        to: Route::Register {},
+                        class: "text-blue-600 hover:underline",
+                        "Register"
                     }
                 }
             }
