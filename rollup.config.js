b9c0fe3bb046b21c16a40ecd26770dd81869745d
Marko Grizelj <marko.grizelj03@gmail.com>
2025-09-09 08:33:56 +0200

    chore: comments

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,15 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'node_modules/mediasoup-client/lib/index.js', // main entry of mediasoup-client
+  input: 'node_modules/mediasoup-client/lib/index.js',
   output: {
     file: 'assets/mediasoup-client.bundle.js',
-    format: 'umd',       // UMD for browser usage
-    name: 'mediasoupClient' // global variable name
+    format: 'umd',
+    name: 'mediasoupClient'
   },
   plugins: [
     resolve({ browser: true }),
     commonjs(),
-    terser() // optional: minify
+    terser()
   ]
 };
