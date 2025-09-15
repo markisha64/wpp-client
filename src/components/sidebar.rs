ecf0ae8cfb017e24cc669ef60a67931c224507a8
Marko Grizelj <marko.grizelj03@gmail.com>
2025-09-15 14:20:17 +0200

    fix: login button

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -121,11 +121,12 @@ pub fn Sidebar(
                 } else {
                     div {
                         class: "flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse",
-                        button {
-                            r#type: "button",
-                            class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800",
-                            Link {
-                                to: Route::Login {}, "Login"
+                        Link {
+                            to: Route::Login {},
+                            button {
+                                r#type: "button",
+                                class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800",
+                                "Login"
                             }
                         }
                     }
